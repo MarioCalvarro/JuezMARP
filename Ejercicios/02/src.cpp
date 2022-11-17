019806fa6a3692e35ce9bcc47a1e9555c7e0b55d
Mario <mcalvarr@ucm.es>
2022-11-17 13:18:30 +0100

    Avances 02

---
+++ Ejercicios/02/src.cpp
@@ -0,0 +1,58 @@
+// DG, Mario Calvarro Marines
+//
+// Solución:
+//
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+
+#include "TreeSet_AVL.h"
+
+bool resuelveCaso() 
+{
+    //Leer
+    size_t N;
+    std::cin >> N;
+
+    if (N == 0)
+        return false;
+
+    Set<size_t> conjunto; 
+    size_t el;
+
+    for (size_t i = 0; i < N; ++i){
+        std::cin >> el; conjunto.insert(el); 
+    }
+
+    size_t M, consulta;
+    std::cin >> M;
+    for (size_t i = 0; i < M; ++i)
+    {
+        std::cin >> consulta;
+        try {
+            std::cout << conjunto.kesimo(consulta) << '\n';
+        } catch(std::domain_error e){
+            std::cout << e.what() << '\n';
+        }
+    }
+
+    std::cout << "---\n";
+
+    return true;
+}
+
+int main() {
+#ifndef DOMJUDGE
+    std::ifstream in("casos.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+    while(resuelveCaso());
+
+#ifndef DOMJUDGE
+    std::cin.rdbuf(cinbuf);
+#endif
+
+    return 0;
+}
